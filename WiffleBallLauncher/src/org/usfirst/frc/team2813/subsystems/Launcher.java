9eb198c285ebcf204a671f615053bf60d78805e7
Omer Sasson <sassonomer123@gmail.com>
2018-11-10 13:45:15 -0800

    Removed WPI_ from Victor motors

--- WiffleBallLauncher/src/org/usfirst/frc/team2813/subsystems/Launcher.java
+++ WiffleBallLauncher/src/org/usfirst/frc/team2813/subsystems/Launcher.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2813.subsystems;
 
 import org.usfirst.frc.team2813.commands.Shoot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -13,7 +13,7 @@ public class Launcher extends Subsystem {
 	
 	private final static int LAUNCHER_MOTOR_ID = 3;
 
-	private final static WPI_VictorSPX launcherMotor = new WPI_VictorSPX(LAUNCHER_MOTOR_ID);
+	private final static VictorSPX launcherMotor = new VictorSPX(LAUNCHER_MOTOR_ID);
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
