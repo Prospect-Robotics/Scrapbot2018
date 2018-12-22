34b510bcd548c0218be7be88b77b3be8d44def46
Ada He <ada.he0908@gmail.com>
2018-12-22 12:51:43 -0800

    Added get distance and get default distance methods

--- WiffleBallLauncher/src/org/usfirst/frc/team2813/subsystems/driveTrain/CreateWPIEncoder.java
+++ WiffleBallLauncher/src/org/usfirst/frc/team2813/subsystems/driveTrain/CreateWPIEncoder.java
@@ -13,17 +13,22 @@ import edu.wpi.first.wpilibj.PIDSourceType;
  */
 public class CreateWPIEncoder {
 	public static class Configuration {
-    	public final double WHEEL_DIAMETER_INCHES = 3.95;
-    	public final double WHEEL_CIRCUMFERENCE_INCHES = Math.PI * WHEEL_DIAMETER_INCHES;
-    	public final double ENCODER_TO_DRIVE_SCALE = 3;
-    	public final double ENCODER_PULSES_PER_ENCODER_REVOLUTION = 256;
-    	public final double ENCODER_PULSES_PER_WHEEL_REVOLUTION = ENCODER_PULSES_PER_ENCODER_REVOLUTION * ENCODER_TO_DRIVE_SCALE;
-    	public final double INCHES_PER_ENCODER_PULSE = WHEEL_CIRCUMFERENCE_INCHES / ENCODER_PULSES_PER_WHEEL_REVOLUTION;  
+		public final double WHEEL_DIAMETER_INCHES = 3.95;
+		public final double WHEEL_CIRCUMFERENCE_INCHES = Math.PI * WHEEL_DIAMETER_INCHES;
+		public final double ENCODER_TO_DRIVE_SCALE = 3;
+		public final double ENCODER_PULSES_PER_ENCODER_REVOLUTION = 256;
+		public final double ENCODER_PULSES_PER_WHEEL_REVOLUTION = ENCODER_PULSES_PER_ENCODER_REVOLUTION
+				* ENCODER_TO_DRIVE_SCALE;
+		public final double INCHES_PER_ENCODER_PULSE = WHEEL_CIRCUMFERENCE_INCHES / ENCODER_PULSES_PER_WHEEL_REVOLUTION;
 	}
-	
+
 	private static final Configuration kDefaultConfiguration = new Configuration();
-	
-public double getDistance(TalonSRX talon, Configuration config, int PIDLoop) {
-	return	talon.getSelectedSensorPosition(PIDLoop)*config.INCHES_PER_ENCODER_PULSE;
-}
+
+	public double getDistance(TalonSRX talon, Configuration config, int PIDLoop) {
+		return talon.getSelectedSensorPosition(PIDLoop) * config.INCHES_PER_ENCODER_PULSE;
+	}
+
+	public double getDefaultDistance(TalonSRX talon, int PIDLoop) {
+		return getDistance(talon, kDefaultConfiguration, PIDLoop);
+	}
 }
